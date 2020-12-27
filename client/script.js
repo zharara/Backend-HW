cd2c5242b1fea331b2c07005d40649ab63a8a274
zharara <zak98h@gmail.com>
2020-12-27 23:44:24 +0300

    first commit

---
+++ client/script.js
@@ -0,0 +1,31 @@
+fetch('http://localhost:3000/greeting?name="zakaria"', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}).then(res => res.json()).then(val => console.log(val))
+    .catch(err => {
+        console.log(err);
+    })
+
+
+fetch('http://localhost:3000/data/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ massage: "hello", name: 'Zakaria' }),
+}).then(res => res.json()).then(val => console.log(val))
+    .catch(err => {
+        console.log(err);
+    })
+
+fetch('http://localhost:3000/greeting/zakaria', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}).then(res => res.json()).then(val => console.log(val))
+    .catch(err => {
+        console.log(err);
+    })
